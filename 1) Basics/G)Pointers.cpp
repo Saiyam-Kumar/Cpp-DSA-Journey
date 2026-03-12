e510222fcd0f4297ad7f250246d4e2cdfa034e87
Saiyam_Kumar <saiyamkumar08@gmail.com>
2026-03-13 00:04:34 +0530

    Update G)Pointers.cpp

--- 1) Basics/G)Pointers.cpp
+++ 1) Basics/G)Pointers.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-// pass by reference by pointers
+// pass by reference using pointer
 void changeA(int *ptr){
     *ptr = 20;
 }
@@ -12,45 +12,40 @@ void ChangeA(int b){
 }
 
 int main(){
-
-    cout<<"--- Basic Pointer Definitions ---"<<endl;
+    // basic pointer definitions
     int a = 10;
     int *ptr = &a;
     int **ptr2 = &ptr;
 
-    cout<< a <<endl;
-    cout<< &a <<endl;
-    cout<< ptr <<endl;
-    cout<< &ptr <<endl;
-    cout<< ptr2 <<endl;
-    cout<< &ptr2 <<endl;
-    cout<< *(&a) <<endl;
-    cout<< *(ptr) <<endl;
-    cout<< *(ptr2) <<endl;
+    cout << a << endl;
+    cout << &a << endl;
+    cout << ptr << endl;
+    cout << &ptr << endl;
+    cout << ptr2 << endl;
+    cout << &ptr2 << endl;
+    cout << *(&a) << endl;
+    cout << *(ptr) << endl;
+    cout << *(ptr2) << endl;
 
-    cout<<"\n--- Null Pointer ---"<<endl;
+    // null pointer
     int *ptr3;
     int *ptr4 = NULL;
+    cout << ptr3 << endl;
+    cout << ptr4 << endl;
 
-    cout<< ptr3 <<endl;
-    cout<< ptr4 <<endl;
-
-    cout<<"\n--- Pass by Reference (Pointer) ---"<<endl;
+    // pass by reference using pointer
     int x = 10;
-
     changeA(&x);
-    cout<< x <<endl;
-
+    cout << x << endl;
     ChangeA(x);
-    cout<< x <<endl;
+    cout << x << endl;
 
-    cout<<"\n--- Pointer Arithmetic ---"<<endl;
+    // pointer arithmetic
     int arr[] = {1,2,3,4};
-
-    cout<< *arr <<endl;
-    cout<< *(arr+1) <<endl;
-    cout<< *(arr+2) <<endl;
-    cout<< *(arr+3) <<endl;
+    cout << *arr << endl;
+    cout << *(arr+1) << endl;
+    cout << *(arr+2) << endl;
+    cout << *(arr+3) << endl;
 
     return 0;
 }
