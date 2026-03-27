d8123189e36535ef77b228ce590a03af96ea14ba
Saiyam_Kumar <saiyamkumar08@gmail.com>
2026-03-27 22:34:49 +0530

    Update B)Maximum_Subarray.cpp

--- 3) Arrays/B)Maximum_Subarray.cpp
+++ 3) Arrays/B)Maximum_Subarray.cpp
@@ -5,6 +5,7 @@ Firstly i done this with brute force used two pointers but this approach gives T
 Optimal Approach-> TC = O(n)
 Kadane's algorithm first i calculated the running sum of array and then store maximum sum and then 
 Reset curr sum -> 0 because negative sum added to any number will decrease the sum more and never be max subarray sum.
+Also True if all elements in array is negative because it already stores the maxSum
 */
 class Solution {
 public:
