b24c866b3ee69fc08638607581bcbb8fd73f57bb
Saiyam_Kumar <saiyamkumar08@gmail.com>
2026-03-30 12:06:58 +0530

    Update C)Majority_Element.cpp

--- 3) Arrays/C)Majority_Element.cpp
+++ 3) Arrays/C)Majority_Element.cpp
@@ -1,1 +1,23 @@
-
+/*
+  Using Moore's Algorithm TC=>O(n) and SC=>O(1)
+  Every element cancels with another except majority element
+  Remaining element will be majority elenent 
+*/
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0,freq = 0; 
+        for(int i=0; i<n; i++){
+            if(freq==0){
+                ans = nums[i];
+            }
+            if(nums[i]==ans){
+                freq++;
+            }else{
+                freq--;
+            }
+        }
+        return ans;
+    }
+};
