bf641a8b5cefec370f1d158a344d8bc8743abd7a
Saiyam_Kumar <saiyamkumar08@gmail.com>
2026-04-13 10:24:18 +0530

    Update B)Maximum_Subarray.cpp

--- 3) Arrays/B)Maximum_Subarray.cpp
+++ 3) Arrays/B)Maximum_Subarray.cpp
@@ -1,11 +1,9 @@
-/* 
-Maximum Subarray Sum -> Leetcode 53
-Brute Approach-> TC = O(n^2)
-Firstly i done this with brute force used two pointers but this approach gives TLE because of larger sums
-Optimal Approach-> TC = O(n)
-Kadane's algorithm first i calculated the running sum of array and then store maximum sum and then 
-Reset curr sum -> 0 because negative sum added to any number will decrease the sum more and never be max subarray sum.
-Also True if all elements in array is negative because it already stores the maxSum
+/*
+    Maximum Subarray Sum -> Leetcode 53
+    Optimal Approach-> TC = O(n)
+    Kadane's algorithm first i calculated the running sum of array and then store maximum sum and then 
+    Reset curr sum -> 0 because negative sum added to any number will decrease the sum more and never be max subarray sum.
+    Also True if all elements in array is negative because it already stores the maxSum
 */
 class Solution {
 public:
