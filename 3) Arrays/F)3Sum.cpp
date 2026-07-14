87b57dd61ae995375ad0ad7c94a6de9169891d60
Saiyam_Kumar <saiyamkumar08@gmail.com>
2026-07-14 14:24:56 +0530

    Create F)3Sum.cpp

---
+++ 3) Arrays/F)3Sum.cpp
@@ -0,0 +1,36 @@
+/*
+3sum - Leetcode - 15
+Sort the array and Use two pointer on the j and k triplets 
+For duplicate triplets use set and get the final triplets whose sum  = 0
+*/
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        set<vector<int>> s;
+        vector<vector<int>> ans;
+        vector<vector<int>> newans;
+        for(int i=0; i<n; i++){
+            int j=i+1,k=n-1;
+            while(j<k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum<0){
+                    j++;
+                }else if(sum>0){
+                    k--;
+                }else{
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++,k--;
+                }
+            }
+        }
+        for(int i=0; i<ans.size(); i++){
+            s.insert(ans[i]);
+        }
+        for(auto it:s){
+            newans.push_back(it);
+        }
+        return newans;
+    }
+};
